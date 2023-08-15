90710f07b7cf201c80eb8a6f90364cad90e4fa34
Shohei Yamasaki <yamasaki0406@gmail.com>
2023-08-15 20:37:42 +0900

    implement crud

--- routes/users.js
+++ routes/users.js
@@ -13,15 +13,82 @@ router.get('/',(req, res, next)=> {
   });
 });
 
+
+
+router.get('/add',(req, res, next)=> {
+  var data = {
+    title: 'Users/Add'
+  }
+  res.render('users/add', data);
+});
+
+router.post('/add',(req, res, next)=> {
+  db.User.create({
+      name: req.body.name,
+      pass: req.body.pass,
+      mail: req.body.mail,
+      age: req.body.age
+    })
+    .then(usr => {
+      res.redirect('/users');
+    });
+});
+
+router.get('/edit',(req, res, next)=> {
+  db.User.findByPk(req.query.id)
+  .then(usr => {
+    var data = {
+      title: 'Users/Edit',
+      form: usr
+    }
+    res.render('users/edit', data);
+  });
+});
+
+router.post('/edit',(req, res, next)=> {
+  db.User.update({
+    name: req.body.name,
+    pass: req.body.pass,
+    mail: req.body.mail,
+    age: req.body.age
+  },
+  {
+    where:{id:req.body.id}
+  })
+  .then(usr => {
+    res.redirect('/users');
+  });
+});
+
+router.get('/delete',(req, res, next)=> {
+  db.User.findByPk(req.query.id)
+  .then(usr => {
+    var data = {
+      title: 'Users/Delete',
+      form: usr
+    }
+    res.render('users/delete', data);
+  });
+});
+
+router.post('/delete',(req, res, next)=> {
+  db.sequelize.sync()
+  .then(() => db.User.destroy({
+    where:{id:req.body.id}
+  }))
+  .then(usr => {
+    res.redirect('/users');
+  });
+});
+
 router.get('/:userId', (req, res) => {
   const id = req.query.userId;
   db.User.findByPk(id).then(usrs => {
     var data = {
-      title: 'Users/Index',
+      title: 'ユーザー詳細画面',
       content: usrs
     }
     res.render('users/index', data);
   });
 })
-
 module.exports = router;
