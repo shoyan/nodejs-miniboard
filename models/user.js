44fc18521a962acc7cc42144afa5ac0e7ac654f1
Shohei Yamasaki <yamasaki0406@gmail.com>
2023-08-15 20:47:27 +0900

    validation

--- models/user.js
+++ models/user.js
@@ -1,26 +1,43 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+  const User = sequelize.define('User', {
+    name: {
+      type:DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: "名前は必ず入力して下さい。"
+        }
+      }
+    },
+    pass: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: "パスワードは必ず入力下さい。"
+        }
+      }
+    },
+    mail: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          msg: "メールアドレスを入力下さい。"
+        }
+      }
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      validate: {
+        isInt: { msg: "整数を入力下さい。"},
+        min: {
+          args: [0],
+          msg: "ゼロ以上の値が必要です。"
+        }
+      }
     }
-  }
-  User.init({
-    name: DataTypes.STRING,
-    pass: DataTypes.STRING,
-    mail: DataTypes.STRING,
-    age: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
   return User;
 };
